9996301bfed1136d17f3ee5ff9e57acf0dca5486
k0na <pwpvpdru@yandex.ru>
2023-06-11 16:18:38 +0300

    lesson_11.4 homework done - add some logic in Main.java - realize new methods in EmailList.java

--- src/main/java/practice/Main.java
+++ src/main/java/practice/Main.java
@@ -1,33 +1,43 @@
-import java.util.Scanner;
+package practice;
+
+import java.util.*;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
-    
-    /* TODO:
-        Пример вывода списка Email, после ввода команды LIST в консоль:
-        test@test.com
-        hello@mail.ru
-        - каждый адрес с новой строки
-        - список должен быть отсортирован по алфавиту
-        - email в разных регистрах считается одинаковыми
-           hello@skillbox.ru == HeLLO@SKILLbox.RU
-        - вывод на печать должен быть в нижнем регистре
-           hello@skillbox.ru
-        Пример вывода сообщения об ошибке при неверном формате Email:
-        "Неверный формат email"
-    */
 
     public static void main(String[] args) {
+        EmailList emailList = new EmailList();
         Scanner scanner = new Scanner(System.in);
-        
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            
-            //TODO: write code here
-            
+
+            input = input.strip();
+            String[] tokens = input.split(" ");
+
+            String command = tokens[0];
+
+            if (tokens.length > 2) {
+                System.out.println(WRONG_EMAIL_ANSWER);
+                continue;
+            }
+
+            switch (command) {
+                case "LIST" -> {
+                    if (!emailList.getSortedEmails().isEmpty()){
+                        for (String s : emailList.getSortedEmails()) {
+                            System.out.println(s);
+                        }
+                    }
+                }
+                case "ADD" -> emailList.add(tokens[1]);
+                default -> System.out.println("Такой команды не найдено");
+            }
+
+
         }
     }
 }
